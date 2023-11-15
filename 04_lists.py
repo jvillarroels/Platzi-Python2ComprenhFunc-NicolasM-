6f012bb0e146f81f57972fc9c6ba46005c905358
Jose Villarroel <josedelvallevs@gmail.com>
2023-11-14 23:31:18 -0500

    Clase 8 Dictionary Comprehension: condition

--- 04_lists.py
+++ 04_lists.py
@@ -7,6 +7,7 @@ print(numbers)
 
 numbers_v2 = [element * 2 for element in range(1,11)]
 print(numbers_v2)
+'''
 
 numbers = []
 for i  in range(1, 11):
@@ -17,15 +18,6 @@ print(numbers)
 
 numbers_v2 = [i * 2 for i in range(1,11) if i % 2 ==0]
 print(numbers)
-'''
-
-names = ['nico', 'zule', 'santi']
-ages = [12, 56, 98]
-
-print(list(zip(names, ages)))
-
-new_dict = {name: age for (name, age) in zip(names, ages)}
-print(new_dict)
 
 
 
