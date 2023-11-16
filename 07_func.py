55b6ca2c3332ffa20472b064af8289998d818864
Jose Villarroel <josedelvallevs@gmail.com>
2023-11-15 21:49:25 -0500

    Clase 11 Funciones

---
+++ 07_func.py
@@ -0,0 +1,18 @@
+print('Hello')
+
+def my_print(text):
+    print(text * 2)
+
+my_print('This is my text ')
+my_print('Hola ')
+
+a = 10 
+b = 90
+
+c = a + b
+
+def suma(a, b):
+    my_print(a + b)
+
+suma(1, 5) # 6   
+suma(10, 4) # 14   
