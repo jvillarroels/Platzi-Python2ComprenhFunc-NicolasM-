8d41d89c08510b380786c53074a89cf1253f6ea5
Jose Villarroel <josedelvallevs@gmail.com>
2023-11-16 23:53:56 -0500

    Clase 12 Funciones:return (III)

--- 08_return.py
+++ 08_return.py
@@ -1,5 +1,5 @@
 def sum_with_range(min, max):
-    print(min, max)
+    print('min = ', min, 'max = ', max)
     sum = 0
     for x in range(min, max):
         sum += x
@@ -7,7 +7,7 @@ def sum_with_range(min, max):
     
 # 1,2,3,4,5,6,7,8,9 = 45
 
-print(sum_with_range(1,10))
+print('sum_with_range(1,10) = ', sum_with_range(1,10))
 result =  sum_with_range(20,30)
 print('result = ', result)
 result_2 =  sum_with_range(result, result + 10)
