880ef93fbe78fe2b00aab1902e2e94e285d4394d
Jose Villarroel <josedelvallevs@gmail.com>
2023-11-18 23:49:03 -0500

    Clase 8 Dictionary Comprehension: condition II

--- 06_dict.py
+++ 06_dict.py
@@ -3,12 +3,12 @@ import random
 countries = ['col', 'mex', 'bol', 'pe']
 
 population_v2 = {country: random.randint(1,100) for country in countries}
-print(population_v2)
+print('population_v2 => ',population_v2)
 
 result = {country: population for (country, population) in 
           population_v2.items() if population > 20}
-print(result)
+print('result => ', result)
 
 text = 'Hola, soy Nicolas'
 unique = { c: c.upper() for c in text if c in 'aeiou'}
-print(unique)
+print('unique => ', unique)
