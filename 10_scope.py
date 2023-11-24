9b91866e59bf2b5aca1421fa5b7c0992cef5d308
Jose Villarroel <josedelvallevs@gmail.com>
2023-11-23 23:45:49 -0500

    Clase 14 El scope II

--- 10_scope.py
+++ 10_scope.py
@@ -4,12 +4,12 @@ def increment():
     price = 200 # local
     price = price + 10
     #result = price + 10
-    print(price)
+    print('price increment() => ', price)
     return price
     #return result
 
 
-print(price)
+print('price global => ', price)
 price_2 = increment()
-print(price_2)
+print('price_2 => ',price_2)
 #print(result)
